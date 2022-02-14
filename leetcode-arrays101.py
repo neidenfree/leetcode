07f10710f14ce02416c6d810f95a866e23da3d0d
neidenfree <avstrelnikov95@gmail.com>
2022-02-14 14:05:32 +0700

    Finished array101 from leetcode

--- leetcode-arrays101.py
+++ leetcode-arrays101.py
@@ -85,15 +85,123 @@ class Solution:
         for i in range(m + n):
             nums1[i] = res[i]
 
+    def removeElement(self, nums: List[int], val: int) -> int:
+        p1 = 0
+        p2 = len(nums) - 1
+        while p1 <= p2:  # Reconsider this!
+            if nums[p2] == val:
+                p2 -= 1
+                continue
+            if nums[p1] == val:
+                nums[p1] = nums[p2]
+                nums[p2] = val
+            else:
+                p1 += 1
+
+        val_count = 0
+        for i in reversed(range(0, len(nums))):
+            if nums[i] == val:
+                val_count += 1
+            else:
+                break
+
+        return len(nums) - val_count
+
+    def removeDuplicates(self, nums: List[int]) -> int:
+        for i in range(len(nums) - 1):
+            if i >= len(nums) - 1:
+                break
+            while nums[i] == nums[i + 1]:
+                del nums[i + 1]
+                if i >= len(nums) - 1:
+                    break
+        return len(nums)
+
+    def checkIfExist(self, arr: List[int]) -> bool:
+        d = {}
+        zeros = 0
+        for elem in arr:
+            if elem == 0:
+                zeros += 1
+            else:
+                d[elem * 2] = 1
+        print(d)
+        if zeros > 1:
+            return True
+        for elem in arr:
+            if elem in d:
+                return True
+        return False
+
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 2:
+            return False
+        changes = 0
+        up = arr[0] < arr[1]
+        if not up:
+            return False
+        for i in range(len(arr) - 1):
+            if arr[i] == arr[i + 1]:
+                return False
+            if up:
+                if arr[i] > arr[i + 1]:
+                    up = False
+                    changes += 1
+            else:
+                if arr[i] < arr[i + 1]:
+                    changes += 1
+        print('c = ', changes)
+        return changes == 1
+
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        if len(arr) < 2:
+            return [-1]
+        local_max = arr[len(arr) - 1]
+        current_max = arr[len(arr) - 1]
+        for i in range(len(arr) - 1, -1, -1):
+            current_max = max(arr[i], current_max)
+            arr[i] = local_max
+            local_max = max(current_max, local_max)
+        arr[len(arr) - 1] = -1
+        return arr
+
+    def moveZeroes(self, nums: List[int]) -> None:
+        slow = 0
+        fast = 0
+        while fast != len(nums):
+            if nums[slow] == 0 and nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+            fast += 1
+            if nums[slow] != 0:
+                slow += 1
+
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        fast = 0
+        slow = 0
+
+        while fast != len(nums):
+            if nums[slow] % 2 != 0 and nums[fast] % 2 == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+            fast += 1
+            if nums[slow] % 2 == 0:
+                slow += 1
+        return nums
+
+    def heightChecker(self, heights: List[int]) -> int:
+        sorted_heights = sorted(heights)
+        res = 0
+        for i in range(len(sorted_heights)):
+            if heights[i] != sorted_heights[i]:
+                res += 1
+        return res
+
 
 
 
 
 if __name__ == '__main__':
     sol = Solution()
-    # arr = [1, 0, 2, 3, 0, 4, 5, 0]
-    nums1 = [1, 2, 3, 0, 0, 0]
-    nums2 = [2, 5, 6]
-    sol.merge(nums1, 3, nums2, 3)
-    print(nums1)
-    # print(arr)
+
+    nums = [3, 2, 2, 4, 6, 5]
+    print(nums)
+    print(sol.sortArrayByParity(nums))
