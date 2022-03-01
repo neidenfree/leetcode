be8c2fd5a539517708d90a9749bb1ba64a4f262a
neidenfree <avstrelnikov95@gmail.com>
2022-03-01 11:05:21 +0700

    Modified path finding algorithm

--- graphs.py
+++ graphs.py
@@ -10,6 +10,16 @@ class Graph:
         if 'dict' in kwargs:
             self.g = kwargs['dict']
             self.m = self.get_matrix()
+        if 'string' in kwargs:
+            self.s: str = kwargs['string']
+            self.g = self.get_graph_dict_from_string()
+
+    def __str__(self):
+        g = self.g
+        s = ""
+        for key in g:
+            s += f'{key}: {g[key]}\n'
+        return s
 
     def get_graph_dict(self) -> Optional[dict]:
         if len(self.m) == 0:
@@ -64,38 +74,73 @@ class Graph:
                 break
         return len(visited) == len(self.g)
 
-    def get_path(self, ind1: int, ind2: int) -> Optional[List[int]]:
+    def get_path(self, start: int, end: int) -> Optional[List[int]]:
+        if start < 0 or end < 0:
+            raise IndexError("Negative index!")
+        if start >= len(self.g) or end >= len(self.g):
+            raise IndexError('One of indices is greater then count of vertices!')
+
         g = self.g
         l = deque()
-        l.append(ind1)
+        l.append(start)
         visited = [False for x in range(len(self.g))]
         prev = [None for x in range(len(self.g))]
-        visited[ind1] = True
+        visited[start] = True
 
         while len(l) is not None:
             current = l.popleft()
             visited[current] = True
             for elem in g[current]:
                 if not visited[elem]:
-                    prev[elem] = current
+                    if prev[elem] is None:
+                        prev[elem] = current
                     l.append(elem)
             if len(l) == 0:
                 break
 
-
         path = []
-        temp = ind2
+        temp = end
         while temp is not None:
             path.append(temp)
             temp = prev[temp]
+        if start not in path:
+            return None
         path.reverse()
+        print(prev)
         return path
 
     def get_matrix(self):
         g = self.g
 
+    def get_graph_dict_from_string(self) -> dict:
+        s = self.s
+        pairs = s.split(';')
+        g = {}
+        for p in pairs:
+            one = p.split(',')[0]
+            try:
+                two = p.split(',')[1]
+            except IndexError:
+                two = None
+            if two is None:
+                if one not in g:
+                    g[one] = []
+            else:
+                one, two = int(one), int(two)
+                if one in g:
+                    g[one].append(two)
+                else:
+                    g[one] = [two]
+                if two in g:
+                    g[two].append(one)
+                else:
+                    g[two] = [one]
+        return g
+
 
 if __name__ == '__main__':
+    graph = Graph(string='0,1; 0,4; 0,2;  4,2; 2,3; 2,6; 2,5; 7')
+
     # graph = Graph(matrix=[
     # [0, 1, 1, 0, 0],
     # [1, 0, 0, 1, 0],
@@ -103,17 +148,21 @@ if __name__ == '__main__':
     # [0, 1, 0, 0, 1],
     # [0, 0, 0, 1, 0],
     # ])
-    graph = Graph(dict={
-        0: [1, 2, 4],
-        1: [0],
-        2: [3, 5, 6],
-        3: [2],
-        4: [0, 2],
-        5: [2],
-        6: [2]
-    })
 
-    print(graph.get_path(1, 6))
+    # graph = Graph(dict={
+    #     0: [1, 2, 4],
+    #     1: [0],
+    #     3: [2],
+    #     2: [0, 4, 3, 5, 6],
+    #
+    #     4: [0, 2],
+    #     5: [2],
+    #     6: [2]
+    # })
+    ind1, ind2 = 6, 4
+    print(f'path from {ind1} to {ind2} =', graph.get_path(ind1, ind2))
+
+    print(graph)
 
     # print(get_path(0, 3, g))
     # assert path_exists(0, 2, g)
